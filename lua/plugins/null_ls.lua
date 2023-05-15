1270cb0b96daf6f06403b2450eb1e84e92a16a81
xclidongbo <xclidongbo@126.com>
2023-05-15 19:02:39 +0800

    lazyVim配置

---
+++ lua/plugins/null_ls.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, {
+        "pyright",
+        "black",
+        "ruff-lsp",
+        "ruff",
+      })
+    end,
+  },
+
+  -- Setup null-ls with `black`
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    opts = function(_, opts)
+      local nls = require("null-ls")
+      opts.sources = vim.list_extend(opts.sources, {
+        nls.builtins.formatting.black,
+        nls.builtins.formatting.ruff,
+        -- nls.builtins.diagnostics.ruff,
+      })
+    end,
+  },
+}
