4f0caaff2b37ed3d6e91f56d2fab484c659402c3
xclidongbo <xclidongbo@126.com>
2023-07-14 09:41:57 +0800

    调整配置

--- lua/plugins/autotag.lua
+++ lua/plugins/autotag.lua
@@ -1,3 +1,9 @@
 return {
-  { "windwp/nvim-ts-autotag" },
+  {
+    "windwp/nvim-ts-autotag",
+    lazy = false,
+    config = function()
+      require("nvim-ts-autotag").setup()
+    end,
+  },
 }
