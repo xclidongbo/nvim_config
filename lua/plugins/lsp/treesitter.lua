31e54fab9c1ba95b760a22f8a2178fa734241076
xclidongbo <xclidongbo@126.com>
2023-09-15 20:10:47 +0800

    无更改

--- lua/plugins/lsp/treesitter.lua
+++ lua/plugins/lsp/treesitter.lua
@@ -2,6 +2,22 @@ return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
     ensure_installed = {
+      "bash",
+      "html",
+      "javascript",
+      "json",
+      "lua",
+      "markdown",
+      "markdown_inline",
+      "python",
+      "query",
+      "regex",
+      "tsx",
+      "typescript",
+      "vim",
+      "yaml",
+      "dockerfile",
+    },
     autotag = {
       enabled = true,
     },
