77d7547c78f33584a71049ccdbbe4cb30a280fe1
xclidongbo <xclidongbo@126.com>
2023-10-31 18:51:21 +0800

    无更改

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,10 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+local auto_command_on = vim.api.nvim_create_autocmd
+
+auto_command_on({ "BufRead", "BufNewFile" }, {
+  pattern = { "*.podspec", "Podfile", "Fastfile", "Appfile", "Matchfile", "Pluginfile" },
+  command = "set filetype=ruby",
+})
